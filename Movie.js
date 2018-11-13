d154b37ac8d2dfeb1f8a7a5d54ad50a43b1240c2
tjming97 <tjming5721@gmail.com>
2018-11-13 16:30:21 +0800

    ready for heroku

--- Movie.js
+++ Movie.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const db = 'mongodb://paul:abc123@ds221242.mlab.com:21242/movies';
+const db = 'mongodb://junming:abc123@ds141043.mlab.com:41043/movie';
 
 mongoose
   .connect(
